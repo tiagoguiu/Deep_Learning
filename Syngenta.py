69a1d428f6d402f62ba4877112c292eef5ed8986
Tiago Rezende Vilas Boas <tiagoguiu98@hotmail.com>
2021-05-26 18:35:37 -0300

    Solução correta

--- Syngenta.py
+++ Syngenta.py
@@ -3,29 +3,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 image = Image.open("Syngenta.bmp")
-
-cores = image.getcolors
-
-print(cores)# o verde esta em 58 do RGB ou seja eu posso contar quais são iguais a 58 pixels
 pixels = list(image.getdata())
-int tam = pixels.count
-cont = 0
-print(tam)
+tam2 = pixels.index(51) #procurando o verde dentro da imagem pelo index RGB
+print(tam2) #QUANTIDADE TOTAL DE PONTOS VERDES 481
 
+#DEIXAREI COMENTADO O QUE EU TENTEI FAZER
+
+#print(tam)
 #for x in tam:
 #    if tam[x] == 58:
 #        cont +=1
-        
-
-
-
-
-
-
-
 #total = len(list(filter(lambda i: i == (50,205,50), pixels)))
 #print("There are %d bright pixels" % total)
 #plt.imshow(image)
 #print(image.size) #420 largura x 300 altura
 #print(image.format) #BMP
 #print(image.mode) #P 
+
+#SEGUNDA PARTE DA ATIVIDADE
+
+
+
+thresh = 200
+fn = lambda x : 255 if x > thresh else 0
+r = image.convert('L').point(fn, mode='1')#nada achado em transformado tudo em branco
+r.show()
