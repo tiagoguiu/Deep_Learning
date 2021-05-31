d4fce7c8befac9d42a028af13ac7d94c2906c71c
Tiago Rezende Vilas Boas <tiagoguiu98@hotmail.com>
2021-05-31 17:35:05 -0300

    last commit

--- Syngenta2.py
+++ Syngenta2.py
@@ -6,7 +6,7 @@ img = cv2.imread("Syngenta.bmp")
 img_color = cv2.cvtColor(img, cv2.COLOR_RGB2HSV_FULL)
 
 ret, thresh = cv2.threshold(img_color, 150, 255, cv2.THRESH_BINARY_INV)
-
+#Alterando a coloração de fundo para branco para ver se encontro
 
 plt.imshow(thresh)
 
