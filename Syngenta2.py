934c61e36ce73acf3869b4a1975204d9c3c1b840
Tiago Rezende Vilas Boas <tiagoguiu98@hotmail.com>
2021-05-31 17:31:02 -0300

    Adicionando parte dois incompleta

---
+++ Syngenta2.py
@@ -0,0 +1,13 @@
+import cv2 
+from matplotlib import pyplot as plt
+
+img = cv2.imread("Syngenta.bmp")
+
+img_color = cv2.cvtColor(img, cv2.COLOR_RGB2HSV_FULL)
+
+ret, thresh = cv2.threshold(img_color, 150, 255, cv2.THRESH_BINARY_INV)
+
+
+plt.imshow(thresh)
+
+plt.show()
