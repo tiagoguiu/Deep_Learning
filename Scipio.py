37a7f579fa0e3948d0b93932d61bc5841a965530
Tiago Rezende Vilas Boas <tiagoguiu98@hotmail.com>
2021-07-07 21:48:10 -0300

    adicionando alterações

--- Scipio.py
+++ Scipio.py
@@ -5,36 +5,29 @@ import numpy as np
 from PIL import Image
 
 
+#abrindo imagem
+image = Image.open("meteor_challenge_01.png")
 
-red = [(255,70,70),(255,0,0)] 
-white = [(170,170,170),(255,255,255)]
-yellow = [(0,240,250),(10,255,255)]
-dot_colors = [red, white, yellow]
-    
-img = cv2.imread('meteor_challenge_01.png')   
-# aplicando medianBlur para suavizar a imagem antes do limiar 
-blur= cv2.medianBlur(img, 7) 
+#pegando os pixels da imagem em RGBA
+pixels = list(image.getdata())
 
-for lower, upper in dot_colors:
-    output = img.copy()
-    # aplicando a cor limite ao branco (255,255, 255) e o resto ao preto (0,0,0)
-    mask = cv2.inRange(blur,lower,upper) 
+#defindo cor de busca
+cor = (255,0,0,255)
 
-    circles = cv2.HoughCircles(mask,cv2.HOUGH_GRADIENT,1,20,param1=20,param2=8,
-                               minRadius=0,maxRadius=60)    
-    index = 0
-    if circles is not None:
-        # convertendo as coordenadas (x, y) e o raio dos círculos em inteiros
-        circles = np.round(circles[0, :]).astype("int")
+#buscando dentro dos pixels quais são vermelho #ff0000 ou pure red
+tam2 = pixels.count(cor)
 
-        # fazendo um loop sobre as coordenadas (x, y) e o raio dos círculos
-        for (x, y, r) in circles:
-            # desenhando o círculo na imagem de saída, 
-            # desenhando um retângulo correspondente ao centro do círculo
-            cv2.circle(output, (x, y), r, (255, 0, 255), 2)
-            cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (255, 0, 255), -1)
+#printando
+print(tam2)
 
-            index = index + 1
-            print (str(index) + " : " + str(r) + ", (x,y) = " + str(x) + ', ' + str(y))
-        print ('No. of circles detected = {}'.format(index))
-print(output)
+#definindo cor branca ou pure white #fffffff
+cor2 = (255,255,255,255)
+
+#bucando dentro dos pixels quais são brancos
+tam = pixels.count(cor2)
+
+#printando
+print(tam)
+
+
+ 
