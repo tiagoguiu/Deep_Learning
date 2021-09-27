2c96fe4a1e54be6c92c1d5f9c0739ba0204727c3
Tiago Rezende Vilas Boas <tiagoguiu98@hotmail.com>
2021-09-27 17:29:35 -0300

    Fixing neural mnist

--- neural_mnist.py
+++ neural_mnist.py
@@ -1,27 +1,35 @@
 import tensorflow as tf
+
 print(tf.__version__)
 
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
-    if(logs.get('loss')<0.01):
+    if(logs.get('loss') <0.01): 
       print("\nAtingiu loss < 0.1, cancelando treinamento")
       self.model.stop_training = True
 
 callbacks = myCallback()
-mnist = tf.keras.datasets.fashion_mnist
-(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
-training_images=training_images/255.0
-test_images=test_images/255.0
+
+mnist = tf.keras.datasets.mnist
+
+(training_x, training_y), (test_images, test_labels) = mnist.load_data()
+training_images = training_x /255.0
+
+test_images = test_images/255.0
+
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(256, activation=tf.nn.relu),
   tf.keras.layers.Dense(128, activation=tf.nn.relu),
-  tf.keras.layers.Dense(16, activation=tf.nn.relu),
+  tf.keras.layers.Dense(64, activation=tf.nn.relu),
+  tf.keras.layers.Dense(32, activation=tf.nn.relu),
 
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax),
 ])
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics=['accuracy'])
-model.fit(training_images, training_labels, epochs=10, callbacks=[callbacks])
+
+model.fit(training_images, training_y, epochs=10, callbacks=[callbacks])
 
 model.evaluate(test_images, test_labels)
 
@@ -31,5 +39,5 @@ print(classifications[0])
 
 print(test_labels[0])
 
-# A acuracia geral foi de 88%, a maior foi 91% foi o maior que consegui
-# Testei varias combinações de multiplas camadas, e com muitos neuronios, mas meu melhor resultado foi esse
+# Utilizei uma rede com 4 camadas no modelo relu e uma camada modelo softMax
+# A acuracia geral foi de 98%, a maior foi 99.34% foi o maior que consegui
