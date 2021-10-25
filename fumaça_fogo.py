d75b82249dd7bf4c40919ca21b2ac65c41fd143a
Tiago Rezende Vilas Boas <tiagoguiu98@hotmail.com>
2021-10-25 20:32:26 -0300

    Adding dropout

--- fumaça_fogo.py
+++ fumaça_fogo.py
@@ -47,38 +47,25 @@ validation_fire_dir = os.path.join(validation_dir, 'fire')
 validation_smoke_dir = os.path.join(validation_dir, 'smoke')
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16, (3, 3), activation='relu',
-                           input_shape=(150, 150, 3)),
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(512, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
-model.compile(optimizer=RMSprop(learning_rate=0.001),
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
+model.compile(optimizer=RMSprop(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(rescale=1.0/255.)
 test_datagen = ImageDataGenerator(rescale=1.0/255.)
 
-train_generator = train_datagen.flow_from_directory(train_dir,
-                                                    batch_size=20,
-                                                    class_mode='binary',
-                                                    target_size=(150, 150))
+train_generator = train_datagen.flow_from_directory(train_dir,batch_size=20,class_mode='binary',target_size=(150, 150))
 
-validation_generator = test_datagen.flow_from_directory(validation_dir,
-                                                        batch_size=20,
-                                                        class_mode='binary',
-                                                        target_size=(150, 150))
+validation_generator = test_datagen.flow_from_directory(validation_dir,batch_size=20,class_mode='binary',target_size=(150, 150))
 
-history = model.fit(train_generator,
-                              validation_data=validation_generator,
-                              steps_per_epoch=100,
-                              epochs=15,
-                              validation_steps=50,
-                              verbose=2)
+history = model.fit(train_generator,validation_data=validation_generator,steps_per_epoch=100,epochs=15,validation_steps=50,verbose=2)
